6af55175b81d406064ddbc20e25b76253bd4f1c7
KreativeNova <kreativenovaa@gmail.com>
2026-07-08 22:30:21 +0530

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,14 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDPb38Um-a9R9qD5IsyVzzi7F9rumVKlNs",
+  authDomain: "kreative-nova.firebaseapp.com",
+  databaseURL: "https://kreative-nova-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "kreative-nova",
+  storageBucket: "kreative-nova.firebasestorage.app",
+  messagingSenderId: "107390460515",
+  appId: "1:107390460515:web:6919a09c4875db5d184e56",
+  measurementId: "G-LJW9JC3LK1"
+};
+
+const IMGBB_API_KEY = "bba08d46811e8dfad272393f5d576d0f";
+const CLOUDINARY_CLOUD = "bweypvel";
+const CLOUDINARY_PRESET = "Kreative Nova";
